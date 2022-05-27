2ab8b6a4d3cf45b66c15a44f61a276dd71fdd938
Hamzah Contreras <Hamzah_36@outlook.com>
2022-05-26 19:01:28 -0500

    Solve or Simplify

---
+++ 4solveOrSimplify.js
@@ -0,0 +1,37 @@
+// Write a function which takes in a string and returns cpunts of each character in the string.
+
+charCount("aaaa")
+// {a: 4}
+
+charCount("hello")
+// {h: 1, e: 1, l: 2, o: 1}
+
+charCount("Your PIN number is 1234!")
+// {1: 1,2: 1, 3: 1, 4: 1, b: 1, e: 1,i: 2, m: 1, n: 2, o: 1, p: 1, r: 2, s: 1, u: 2, y: 1}
+
+function charCount(str) {
+   // do something
+   // return an object with keys that are lowercase alphanumeric characters in the string; values should be the counts for those characters.
+}
+
+function charCount(str) {
+   // make object to return at end
+   let count = {}
+   // loop over string, for each character...
+   for (let i = 0; i < str.length; i++) {
+      let char = str[i].toLowerCase()
+      // if the character is a number/letter AND is a key in object, add one to count
+      if (count[char] > 0) {
+         count[char]++
+      }
+      // if the character is a number/letter AND not in object, add it to object and set value to 1
+      else {
+         count[char] = 1
+      }
+   }
+      // if character is something else (space, period, etc.) don't do anything
+   // return object at end
+   return count
+}
+
+console.log(charCount("Your PIN number is 1234!"))
